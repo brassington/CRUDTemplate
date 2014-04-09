070a1eb3b509d096afbe21d87a62ee98f6a80293
J. Andrew Brassington <jabbrass@zoho.com>
2014-04-09 16:41:15 -0500

    Initial Commit

---
+++ CRUDTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUDTemplate.Startup))]
+namespace CRUDTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
